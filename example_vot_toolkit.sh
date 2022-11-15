1dca8bd74f36864c8c0668bf874f8d9fb7f0f18f
franktpmvu <38975575+franktpmvu@users.noreply.github.com>
2022-11-15 16:07:19 +0800

    Update example_vot_toolkit.sh

--- example_vot_toolkit.sh
+++ example_vot_toolkit.sh
@@ -2,7 +2,7 @@ mkdir vot
 cd vot
 mkdir vot2022stb
 vot initialize vot2022/stb --workspace vot2022stb
-cp trackers/ostrack/trackers.ini ./
+cp ../trackers/ostrack/trackers.ini ./
 'change path of trackers.ini'
 vot test ostrackNeighbor
 vot evaluate --workspace vot2022stb ostrackNeighbor
